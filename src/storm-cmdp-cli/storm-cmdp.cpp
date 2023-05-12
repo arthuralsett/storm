dd07489005ec1791f1fb40daf2b93bccf0ec7a0a
Arthur Al-Sett <arthuralsett+vc.storm@gmail.com>
2023-05-12 17:12:27 +0200

    Remove useless old code

--- src/storm-cmdp-cli/storm-cmdp.cpp
+++ src/storm-cmdp-cli/storm-cmdp.cpp
@@ -36,12 +36,8 @@ namespace storm {
         typedef storm::cli::SymbolicInput SymbolicInput;
 
         void processInput(SymbolicInput &input, storm::cli::ModelProcessingInformation& mpi) {
-            std::cout << "Hello, world.\n";
             auto model = storm::cli::buildPreprocessExportModelWithValueTypeAndDdlib<storm::dd::DdType::CUDD, double>(input, mpi);
             auto cmdp = model->template as<storm::models::sparse::Mdp<double>>();
-            // std::cout << "cmdp->getNumberOfStates() = " << cmdp->getNumberOfStates() << '\n';
-            // std::cout << "cmdp->getNumberOfTransitions() = " << cmdp->getNumberOfTransitions() << '\n';
-            // std::cout << "cmdp->getNumberOfChoices() = " << cmdp->getNumberOfChoices() << '\n';
 
             auto minInitConsWrongOrder = computeMinInitCons(cmdp);
 
